fd21df0fdd26143128123a0c1ba3780521329419
jerome suyat <jerome.suyat@intellisolution.tech>
2024-10-18 14:38:28 +0800

    setup

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,9 +27,14 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->darkMode(false)
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Blue,
             ])
+            // ->favicon(asset('images/favicon.png'));
+            ->brandName('Intelli Street Light')
+            // ->brandLogo(fn() => view('filament.admin.logo'))
+            // ->brandLogoHeight('1rem')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
